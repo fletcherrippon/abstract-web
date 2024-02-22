e3495abb055380b1e408954d64d7665070f9c1c3
Fletcher Rippon <fletcherrippon@gmail.com>
2024-02-22 18:58:36 +1100

    added base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/abstract-web/",
   plugins: [svelte(), glsl()],
   css: {
     postcss,
